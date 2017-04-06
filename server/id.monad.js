e03507a47c8ce18aa7f0af60002c0ddd34d47723
Andrew Jarrett <ahrjarrett@gmail.com>
2017-04-06 13:43:22 -0600

    add identity for the server

--- server/id.monad.js
+++ server/id.monad.js
@@ -1,1 +1,16 @@
 // Identity on the server
+
+const Monad = () => {
+  return function(value) {
+    let monad = Object.create(null)
+    monad.bind = (fn) => fn(value)
+    return monad
+  }
+}
+
+// Example Usage:
+const identity = Monad()
+const monad = identity('hello wurld')
+monad.bind(console.log)
+
+module.exports = Monad
