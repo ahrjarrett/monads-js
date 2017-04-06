6aef8199bd6043476744117b4199b55788d69e49
Andrew Jarrett <ahrjarrett@gmail.com>
2017-04-06 14:05:03 -0600

    modified server id M to add more than 1 arg (which would be the value passed into the monad

--- server/id.monad.js
+++ server/id.monad.js
@@ -1,16 +1,18 @@
 // Identity on the server
 
 const Monad = () => {
+  let prototype = Object.create(null)
   return function(value) {
-    let monad = Object.create(null)
-    monad.bind = (fn) => fn(value)
+    let monad = Object.create(prototype)
+    monad.bind = (fn, ...args) => fn(value, ...args)
     return monad
   }
 }
 
-// Example Usage:
-const identity = Monad()
-const monad = identity('hello wurld')
-monad.bind(console.log)
+//// Example Usage:
+//const identity = Monad()
+//const monad = identity('hello wurld')
+//monad.bind(console.log, 'it’s me, the identity monad')
+//// => hello wurld it’s me, the identity monad
 
 module.exports = Monad
