ac35263ed43ffcac605fc284335a1f810479ef26
Andrew Jarrett <ahrjarrett@gmail.com>
2017-04-06 17:10:19 -0600

    broke monad for client, returns 1st char of 2nd param??

--- client/id.monad.js
+++ client/id.monad.js
@@ -3,16 +3,19 @@
 // Monad returns a function "unit" that takes a value that replaces the null in the object monad that is bound to the function "bind"
 // This allows us to initialize the object and pass in functions that will return some kind of operation or procedure on "value"
 function Monad() {
+  var prototype = Object.create(null)
   return function unit(value) {
-    var monad = Object.create(null)
-    monad.bind = function(func) {
-      return func(value)
+    var monad = Object.create(prototype)
+    monad.bind = function(func, args) {
+      return func.apply(undefined,
+        [].concat(Array.prototype.slice.apply(args || [])))
     }
     return monad
   }
 }
 
 // Example use:
+
 //var identity = Monad()
-//var monad = identity("Hello wurld")
-//monad.bind(alert) // Alerts "Hello wurld"
+//var monad = identity("Hello")
+//monad.bind(alert, "cruel wurld") // Alerts "Hello cruel world"
